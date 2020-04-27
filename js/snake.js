c952bdb6fea82a4c0f4c0ce8d69ff1b74c8febdb
JunJun435 <50116354+JunJun435@users.noreply.github.com>
2020-04-27 12:05:12 -0400

    variables and stuff

--- js/snake.js
+++ js/snake.js
@@ -1,27 +1,21 @@
-const ct = document.getElementById("snakeboard");
-const ct = cv.getContext("2d");
+var snakeboard = document.getElementById("snakeboard");
+var header = document.getElementById("header");
 
-ct.strokeStyle = "#fff";
-ct.strokeRect(0, 0, 600, 600);
+var totalRows = 17;
+var totalRows = 17;
 
-let snake = [];
-let length = 40;
-let score = 0;
-let direction;
+var stageDivider = []
 
-snake [0] = {
-  x: 5 * length,
-  y: 4 * length
-};
+var blockSize = 30;
 
-let prey = {
-  x: Math.floor(Math.random() * 5 + 1) * length,
-  y: Math.floor(Math.random() * 6 + 1) * length
-};
+/* functions*/
 
-function getFrame(){
-
-
+function init()
+{
+    createStageDividers();
 }
 
-let snakeGame = setInterval(getFrame, 200)
+function createStageDividers()
+{
+
+}

da80b9e426f8e083ff4f88995c174057047007c9
JunJun435 <50116354+JunJun435@users.noreply.github.com>
2020-04-27 11:04:27 -0400

    started js

--- js/snake.js
+++ js/snake.js
@@ -1,1 +1,10 @@
+const ct = document.getElementById("snakeboard");
+const ct = cv.getContext("2d");
 
+ct.strokeStyle = "#fff";
+ct.strokeRect(0, 0, 600, 600);
+
+let snake = [];
+let length = 40;
+let score = 0;
+let direction;

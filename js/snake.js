32bd53d3ef469641f4412e88dfa7a6c5bf04adb2
JunJun435 <50116354+JunJun435@users.noreply.github.com>
2020-04-27 16:16:57 -0400

    some issue with positioning

--- js/snake.js
+++ js/snake.js
@@ -1,12 +1,14 @@
 var board = document.getElementById("board");
 var header = document.getElementById("header");
 
-var totalRows = 17;
-var totalColumns = 17;
+var totalRows = 19;
+var totalColumns = 19;
 
 var boardDiv = []
 
 var blockSize = 30;
+
+init();
 
 /* functions*/
 
@@ -17,7 +19,32 @@ function init()
 
 function createBoardDivs()
 {
+  /* this function is used to calculate the width and height based on the # of rows & columns*/
   board.style.width = blockSize * totalColumns + "px";
-  board.style.height = bloackSize * totalRows + "px";
+  board.style.height = blockSize * totalRows + "px";
+
+  boardDiv = [];
+
+  for (var row = 0; row < totalRows; row++) {
+
+    boardDiv[row] = [];
+
+    for (var column = 0; column < totalColumns; column++) {
+
+        boardDiv[row][column] = document.createElement("div");
+        board.appendChild(boardDiv[row][column]);
+
+        boardDiv[row][column].style.position = "absolute";
+        boardDiv[row][column].style.left = (column * blockSize) + "px";
+        boardDiv[row][column].style.top = (row * blockSize) + "px"
+
+        boardDiv[row][column].style.width = blockSize + "px";
+        boardDiv[row][column].style.height = blockSize + "px";
+
+        boardDiv[row][column].style.border = "1px solid black";
+        boardDiv[row][column].style.boxSizing = "border-box"
+    }
+
+  }
 
 }
